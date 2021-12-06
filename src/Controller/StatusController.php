07c2b9d5d08ef9b3abf8b733181aff910b7b8582
Skyferix <patrycjusz.august@gmail.com>
2021-12-06 23:04:30 +0200

    feat project-create page and fronteend logic handle

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -5,18 +5,28 @@ namespace App\Controller;
 use App\Entity\Project;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StatusController extends AbstractController
 {
     #[Route('/status', name: 'status')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
-//        $project = $doctrine->getRepository(Project::class)->find();
-//        if(!$project){
-//            return
-//        }
+        $projects = $doctrine->getRepository(Project::class)->findAll();
+        if($request->isMethod("POST")){
+            if($request->get("title")==""){
+                $this->addFlash('error', "Some of the fields are empty please check");
+            }
+        }
+        if(!$projects){
+            return $this->render('create-project.html.twig');
+        }
         return $this->render('status.html.twig');
     }
+
+//    public function submit(){
+//        var
+//    }
 }
